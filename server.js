3624df5987e130782b8118340714b641be1a605d
pankecal <pankaj.ecal@gmail.com>
2023-05-09 18:07:51 +0530

    implement OAuth2.0 with passport.js

--- server.js
+++ server.js
@@ -3,20 +3,33 @@ const https = require("https");
 const express = require("express");
 const helmet = require("helmet");
 const path = require("path");
+const passport = require("passport");
+const { Strategy } = require("passport-google-oauth20");
 require("dotenv").config();
 
-console.log(process.env.CLIENT_ID); // remove this after you've confirmed it is working
-console.log(process.env.CLIENT_SECRET); // remove this after you've confirmed it is working
+const PORT = 3000;
 
 const config = {
 	CLIENT_ID: process.env.CLIENT_ID,
 	CLIENT_SECRET: process.env.CLIENT_SECRET,
 };
 
-const PORT = 3000;
+const AUTH_OPTIONS = {
+	callbackURL: `/auth/google/callback`,
+	clientID: config.CLIENT_ID,
+	clientSecret: config.CLIENT_SECRET,
+};
+
+function verifyCallback(accessToken, refreshToken, profile, done) {
+	done(null, profile);
+}
+
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
+
 const app = express();
 
 app.use(helmet());
+app.use(passport.initialize()); // initialize passport middleware
 
 function checkLoggedIn(req, res, next) {
 	const isLoggedIn = true; // TODO
@@ -30,9 +43,24 @@ function checkLoggedIn(req, res, next) {
 	next();
 }
 
-app.get("/auth/google", (req, res) => {});
+app.get(
+	"/auth/google",
+	passport.authenticate("google", {
+		scope: ["email"], // which data is requested if request is successful
+	})
+);
 
-app.get("/auth/google/callback", (req, res) => {});
+app.get(
+	"/auth/google/callback",
+	passport.authenticate("google", {
+		failureRedirect: "/auth/failure",
+		successRedirect: "/",
+		session: false,
+	}),
+	(req, res) => {
+		console.log("Google called us back!");
+	}
+);
 
 app.get("/auth/logout", (req, res) => {});
 
@@ -43,6 +71,10 @@ app.get("/", checkLoggedIn, (req, res) => {
 
 app.get("/secret", checkLoggedIn, (req, res) => {
 	return res.status(200).send("<h1>Your secret is 85</h1>");
+});
+
+app.get("/failure", checkLoggedIn, (req, res) => {
+	return res.send("<h1>Failed to login </h1>");
 });
 
 https
