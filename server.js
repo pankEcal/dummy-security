d54b088bf47edd80c488a0d2222480541b9a1ea6
pankecal <pankaj.ecal@gmail.com>
2023-05-09 15:05:29 +0530

    get project ready to implement OAuth

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ const fs = require("fs");
 const https = require("https");
 const express = require("express");
 const helmet = require("helmet");
+const path = require("path");
+require("dotenv").config();
+
+console.log(process.env.CLIENT_ID); // remove this after you've confirmed it is working
+console.log(process.env.CLIENT_SECRET); // remove this after you've confirmed it is working
+
+const config = {
+	CLIENT_ID: process.env.CLIENT_ID,
+	CLIENT_SECRET: process.env.CLIENT_SECRET,
+};
 
 const PORT = 3000;
 const app = express();
@@ -27,7 +37,12 @@ app.get("/auth/google/callback", (req, res) => {});
 app.get("/auth/logout", (req, res) => {});
 
 app.get("/", checkLoggedIn, (req, res) => {
-	return res.status(200).send({ status: 200, message: "Hello from HTTPS" });
+	const frontEndFile = path.join(__dirname, "/public/index.html");
+	return res.status(200).sendFile(frontEndFile);
+});
+
+app.get("/secret", checkLoggedIn, (req, res) => {
+	return res.status(200).send("<h1>Your secret is 85</h1>");
 });
 
 https
